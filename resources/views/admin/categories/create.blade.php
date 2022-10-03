d52496cac5040250ff55e5bd1c7bdf325fbc3e6d
Mukhammadali Dadabaev <ali_6017@mail.ru>
2022-10-03 20:07:15 +0500

    Post-yozish bazaga

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -8,7 +8,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
+                    <h1 class="m-0">Добавление категории</h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -26,11 +26,17 @@
         <div class="container-fluid">
             <!-- Small boxes (Stat box) -->
             <div class="row">
-                <div class="col-2">
-                    <a href="{{ route('admin.category.create') }}" class="btn btn-block btn-primary">Добавить</a>
-                </div>
-                <div class="col-12">
-                    Категории
+               <div class="col-12">
+                   <form action="{{ route('admin.category.store') }}" method="POST" class="w-25">
+                       @csrf
+                       <div class="form-group">
+                           <input type="text" class="form-control" name="title" placeholder="Название категории">
+                           @error('title')
+                                <div class="text-danger">To`ldirish shartbr 👆<br> {{ $message }}</div>
+                           @enderror
+                       </div>
+                       <input type="submit" class="btn btn-primary" value="Добавить">
+                   </form>
                 </div>
                 <!-- ./col -->
             </div>
